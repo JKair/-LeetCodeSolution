c6cb05a88d8f7d976d55604fbc1ebb8aafe7fb51
kair <1342830239@163.com>
2015-06-03 16:27:38 +0800

    Course Schedule

---
+++ 2.Medium/Course Schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    # @param {integer} numCourses
+    # @param {integer[][]} prerequisites
+    # @return {boolean}
+    def canFinish(self, numCourses, prerequisites):
+    	if not prerequisites:
+    		return True
+        indegrees = []
+        result = []
+        edges = 0
+        zeroIndeg = []
+        for i in xrange(numCourses):
+        	indegrees.append(0)
+        for num in prerequisites:
+        	indegrees[num[0]] = indegrees[num[0]] + 1
+        	edges = edges + 1
+        for i in xrange(len(indegrees)):
+        	if indegrees[i] == 0:
+        		zeroIndeg.append(i)
+        while len(zeroIndeg) != 0:
+        	view = zeroIndeg.pop()
+        	for i in prerequisites:
+        		if i[1] == view:
+        			edges = edges - 1
+        			indegrees[i[0]] = indegrees[i[0]] - 1
+        			if indegrees[i[0]] == 0:
+        				zeroIndeg.append(i[0])
+        if edges == 0:
+        	return True
+        else:
+        	return False
+a = Solution()
+print a.canFinish(3, [[1,0],[2,1]])
