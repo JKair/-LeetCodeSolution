4facb3938e497c556fccecac1821af99c7c9b308
kair <1342830239@163.com>
2015-06-05 14:13:34 +0800

    Count Primes

---
+++ 1.Easy/Count Primes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+	int countPrimes(int n) {
+		bool flag = new bool[n];
+	}
+};
+void main()
+{
+	bool flag = new bool;
+	cout << flag;
+}
